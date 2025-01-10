84bce332661d2fd4d6692af2c0798331200062bb
Komal Meena <capkomal2203@gmail.com>
2025-01-10 18:05:58 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"//treats all js code as newer version
+
+// alert("komal")//wont work here as we are using nodejs ,not browser
+//code readability is important
+  
+let name = "Komal"
+let age = 21
+let isLoggedIn = true
+
+
+//data types
+//number => 2 to the power 53 aprrox
+//bigint=>for very large numbers
+//string => "" or '' both are valid//but "" is preferred
+// boolean => true or false
+//null => it is a standalone value // representation of empty value
+//undefined => when value not defined 
+//symbol =>mostly used for uniqueness
+
+
+//object
+console.log(typeof null)//null is an object
+console.log(typeof undefined)//undefined -> data type
+
+//console.log() => by default prints each command on new line
