fd09b7e47af12cbdde0cad17723224e6c0b088a5
Komal Meena <capkomal2203@gmail.com>
2025-01-15 15:26:47 +0000

    object1

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -16,7 +16,8 @@ const mySym = Symbol("key1")
 const JsUser = {
     name: "komal",
     "full name":"komal meena",//also correct
-    [mySym]: "mykey1",
+    [mySym]: "mykey1",//square brackets are to be used for symbols 
+    //or it will be read as a normal key
     age: 21,
     location: "delhi",
     email: "komal@gmail.com",
@@ -24,8 +25,38 @@ const JsUser = {
     lastLoginDays: ["Monday", "Thursday"]
 }
 
-//behind the scenes these keys are stores as strings
-console.log(JsUser["name"])//to access the value of key = email
-console.log(JsUser["full name"])//this cant be accessed using dot method as below
+// //behind the scenes these keys are stores as strings
+// console.log(JsUser["name"])//to access the value of key = email
+// console.log(JsUser["full name"])//this cant be accessed using dot method as below
 
-console.log(JsUser.email)//a way of accessing objects
+// console.log(JsUser.email)//a way of accessing objects
+// console.log(JsUser[mySym])//printing a symbol
+
+// JsUser.email = "komalmeena@gmail.com"
+// console.log(JsUser["email"])//overwriting value of email
+
+
+
+
+// Object.freeze(JsUser)///no more changes can be done to this object now
+// JsUser.email = "komalkomal@gmail.com"
+// console.log(JsUser["email"])//email didn't get updated because jsuser was frpzen
+// console.log(JsUser)
+
+
+
+JsUser.greeting = function(){
+    console.log("Hello everyone!")
+}
+
+// console.log(JsUser.greeting)///prints function[anonymous]
+// console.log(JsUser.greeting())//gives output of function
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS User, ${this.name}`)//this -> is used when we want to referance the same object 
+    //it will help us to access the properties of the current object 
+}
+
+JsUser.greetingTwo();//gives output of function
+
+console.log(JsUser.greetingTwo())//gives output of function and also prints undefined
---
+++ 02_basics/04_objects.js
@@ -0,0 +1,25 @@
+// objects part 2
+
+// // const tinderUser = new Object()//singleton object
+// // or
+// const tinderUser = {} //non-singleton object 
+
+// tinderUser.id = "123abc"
+// tinderUser.name = "Sammy"
+// tinderUser.isLoggedIn = false
+
+// //same output
+// console.log(tinderUser)//{}
+
+
+
+const regularUser ={
+    email: "some@gmail.com",
+    fullname : {
+        userfullname:{
+            firstname: "komal",
+            lastname: "meena"
+        }
+
+    }
+}
