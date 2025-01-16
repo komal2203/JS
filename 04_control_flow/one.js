d1960524a77cae19d750a98341280f25d483dd52
Komal Meena <capkomal2203@gmail.com>
2025-01-16 12:55:14 +0000

    commmitt

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,2 +1,56 @@
 ///control flow or say logic flow
 
+// const isUserLoggedIn = true
+// if(isUserLoggedIn){
+//     console.log(`logged in`)
+// }
+
+// const temperature = 89
+// if(temperature<50){
+//     console.log(`temp below 50`)
+// }else{
+//     console.log(`temp above 50`)
+// }
+
+// if(2=='2'){
+//     console.log(`i am happy`)
+// }else{
+//     console.log(`i am very happy`)
+// }
+
+// // conditional operators
+// // <=,>=,==,===,<,>,!=,!==
+// ///=== is known as strict equal
+
+
+// const score = 200
+
+// if(score>100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`)
+// }
+// // console.log(`User power: ${power}`)//power not defined here
+
+
+// const balance = 673
+
+// // if(balance>500) console.log(`test`)///implicit scope
+
+// if(balance<500){
+//     console.log(`less than 500`)
+// }else if(balance<750){
+//     console.log(`less than 750`)
+// }else if(balance < 900){
+//     console.log(`less than 900`)
+// }else{
+//     console.log(`greater than 900`)
+// }
+
+
+
+const userLoggedIn = true
+const debitCard = true
+
+if(userLoggedIn && debitCard){
+    console.log(`allowed to buy courses
+}
