c61da99bf56ceea8f1173b91ae10d7e9574424d2
Komal Meena <capkomal2203@gmail.com>
2025-01-22 13:52:13 +0000

    bindd

--- 10_classes_oop/staticprop.js
+++ 10_classes_oop/staticprop.js
@@ -1,3 +1,29 @@
-class User{
-    
+class User {
+    constructor(username) {
+        this.username = username
+    }
+
+    logMe() {
+        console.log(`Username : ${this.username}`)
+    }
+
+    static createId() {//static doesnt give access of this function to every instance of user class
+
+        return `123`
+    }
 }
+const komal = new User("komal")
+// console.log(komal.createId())
+
+class Teacher extends User {
+    constructor(username, email) {
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+console.log(iphone)
+iphone.logMe()
+
+// console.log(iphone.createId())//wont give output as createId is a static function
