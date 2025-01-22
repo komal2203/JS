30be2f44bf9078b5397503f84e42f5a9b4edc49f
Komal Meena <capkomal2203@gmail.com>
2025-01-22 06:04:42 +0000

    dine

---
+++ 10_classes_oop/prototype.js
@@ -0,0 +1,6 @@
+let myName = "komal   "
+let myChannel = "coffee   "
+console.log(myName.trim().length)
+// whitespace characters are characters used to represent spaces, tabs, line breaks, and other invisible spacing.
+
+///will try to make a function ->  truelength()
